cf15902fe3b882e6a9150ca3f17b65033b5afa09
Doohae <rick7213@gmail.com>
2021-12-12 05:58:54 +0000

    Fold Modified : test.py

--- code/test.py
+++ code/test.py
@@ -5,24 +5,52 @@ import numpy as np
 from tqdm.auto import tqdm
 from dataloader import KistDataset
 import os
+from glob import glob
 from model import CompareNet
 import argparse
 
 
+# Inference Part
+def inference(model, test_data_loader, device, idx):
+    test_value = []
+    description = f"Fold-{idx+1}"
+    with torch.no_grad():
+        model.eval()
+        for test_before, test_after in tqdm(test_data_loader, desc=description):
+            test_before = test_before.to(device)
+            test_after = test_after.to(device)
+
+            logit = model(test_before, test_after)
+            value = logit.squeeze(1).detach().cpu().float()
+
+            test_value.extend(value)
+
+    torch_label = torch.FloatTensor(test_value)
+    np_label = torch_label.numpy()
+
+    return np_label
+
+
+
+def get_kfold_model_path(fold_path):
+    fold_model_pathes = []
+    each_fold = glob(os.path.join(fold_path, "*"))
+    for fold in each_fold:
+        if "fold" in fold:
+            fold_model_pathes.extend(glob(os.path.join(fold, "*")))
+    fold_model_pathes = [path for path in fold_model_pathes if path.endswith(".pt")]
+    return fold_model_pathes
+
+
+
 def main(args):
+
     TEST_PATH = "../data/test_dataset/"
-    # MODEL_PATH = os.path.join("./exp/", args.model_name)
-    MODEL_PATH = "/opt/ml/dacon_baesline/code/exp/2021-12-11-16:56/fold1/checkpoint-fold1-28.pt"
-    assert os.path.exists(MODEL_PATH), "Wrong Model Path"
+    FOLD_PATH = os.path.join("./exp/", args.fold_path) if os.path.isdir(os.path.join("./exp/", args.fold_path)) else args.fold_path
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    assert os.path.isdir(FOLD_PATH), "Wrong Model Path"
     assert args.submission_file.split(".")[-1] == "csv", "Wrong Output File Name"
     assert args.label_type.lower() in ["int", "float"], "Choose Label Type : int or float"
-
-    # Load Model
-    model = CompareNet()
-    checkpoint = torch.load(MODEL_PATH)
-    model.load_state_dict(checkpoint['model'])
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    model = model.to(device)
 
     # Make Inference Data
     test_set = pd.read_csv(os.path.join(TEST_PATH, 'test_data.csv'))
@@ -38,27 +66,27 @@ def main(args):
     test_data_loader = DataLoader(test_dataset,
                                 batch_size=64,
                                 shuffle=False)
-    test_value = []
-    with torch.no_grad():
-        model.eval()
-        for test_before, test_after in tqdm(test_data_loader):
-            test_before = test_before.to(device)
-            test_after = test_after.to(device)
 
-            logit = model(test_before, test_after)
-            value = logit.squeeze(1).detach().cpu().float()
+    # Get Fold Model Pathes
+    model_list = get_kfold_model_path(FOLD_PATH)
 
-            test_value.extend(value)
+    # Iteration : Model Load and Inference
+    sum_labels = np.zeros(len(test_set))
+    for idx, path in enumerate(model_list):
+        # Load Model
+        model = CompareNet()
+        checkpoint = torch.load(path)
+        model.load_state_dict(checkpoint['model'])
+        model = model.to(device)
+        sum_labels = sum_labels + inference(model, test_data_loader, device, idx)
+    mean_labels = sum_labels/len(model_list)
 
-    torch_label = torch.FloatTensor(test_value)
-
-    np_label = torch_label.numpy()
-    np_label[np.where(np_label<1)] = 1
+    mean_labels[np.where(mean_labels<1)] = 1
 
     if args.label_type.lower() == "int":
-        label = [round(n) for n in np_label]
+        label = [round(n) for n in mean_labels]
     elif args.label_type.lower() == "float":
-        label = np_label
+        label = mean_labels
 
     new_sub = pd.DataFrame({
         "idx" : submission['idx'],
@@ -69,7 +97,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="inference setter")
-    parser.add_argument("--model_name", type=str, default=None)
+    parser.add_argument("--fold_path", type=str, default=None)
     parser.add_argument("--submission_file", type=str, default="./submission.csv")
     parser.add_argument("--label_type", type=str, default="int")
     args = parser.parse_args()
--- code/train.sh
+++ code/train.sh
@@ -1,4 +1,4 @@
 python train.py --lr 2e-5 \
-                --epochs 20 \
+                --epochs 30 \
                 --batch_size 64 \
                 --seed 47 
