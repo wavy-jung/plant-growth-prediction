fa78edbcf5af49f3adfef3cd796a153e12e75ca1
Doohae <rick7213@gmail.com>
2021-12-12 18:38:59 +0000

    Shell for Testing

---
+++ code/test.sh
@@ -0,0 +1,7 @@
+python test.py \
+    --fold_path 2021-12-12-15-21 \
+    --pretrained tf_efficientnet_b0_ns \
+    --submission_file tf_efficientnet_b0_ns_fold30.csv \
+    --label_type float \
+    --inf_type fold \
+    # --single_path ckpt
