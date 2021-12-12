dc3b4e78808fceda02f489b34193dea0de90b97d
Doohae <rick7213@gmail.com>
2021-12-12 18:40:31 +0000

    Train: exp

--- code/train.sh
+++ code/train.sh
@@ -1,4 +1,5 @@
-python train.py --lr 2e-5 \
-                --epochs 30 \
+python train.py --pretrained tf_efficientnet_b2_ns \
+                --lr 2e-5 \
+                --epochs 20 \
                 --batch_size 64 \
                 --seed 47 
