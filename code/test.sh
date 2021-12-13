e030caf920ceeb421eca2dd27be125513a4d4d8b
Doohae <rick7213@gmail.com>
2021-12-13 18:43:58 +0000

    Exp

--- code/test.sh
+++ code/test.sh
@@ -1,5 +1,6 @@
-python test.py --fold_path 2021-12-13-7-51 \
-                --pretrained regnetx_032 \
-                --submission_file regnetx_032_fold-2.csv \
-                --label_type float \
+python test.py --fold_path 2021-12-13-13-0 \
+                --pretrained regnetx_040 \
+                --submission_file regnetx_040_fold20.csv \
                 --inf_type single \
+                --label_type float \
+                --single_path /opt/ml/dacon_baseline/code/exp/2021-12-13-13-0/fold2/checkpoint-fold2-20.pt
--- code/train.sh
+++ code/train.sh
@@ -1,5 +1,5 @@
-python train.py --pretrained regnetx_040 \
+python train.py --pretrained regnety_032 \
                 --lr 3e-5 \
-                --epochs 20 \
+                --epochs 30 \
                 --batch_size 64 \
                 --seed 47 
